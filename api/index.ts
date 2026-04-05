86ce0be63bdf0b0ae800d4fb7ee55a14824266f7
DatPHP <nguyenvandat170296@gmail.com>
2026-04-05 08:28:04 +0800

    Fix login

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,18 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { prisma } from '../src/lib/prisma.ts';
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 const app = express();
 app.use(express.json());
